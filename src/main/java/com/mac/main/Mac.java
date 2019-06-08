0a07fd344842e07c72c374bb2083ecd086125a2a
javi <javoraptor@gmail.com>
2019-06-08 11:44:21 -0500

    other commit

--- src/main/java/com/mac/main/Mac.java
+++ src/main/java/com/mac/main/Mac.java
@@ -2,7 +2,23 @@ package com.mac.main;
 
 
 public class Mac {
+
     public static void main(String[]  args){
-        System.out.println("hello world!");
+
+        // String , Integer , Boolean ,
+
+        String initialWord = "abcDeFghiJkLLLLmno";
+        int myInt = 5;
+        Integer myInteger = 5;
+
+        loopThroughString();
+
+    }
+
+    public static void loopThroughString() {
+//        for (){
+//
+//        }
+        System.out.println("this is working");
     }
 }
