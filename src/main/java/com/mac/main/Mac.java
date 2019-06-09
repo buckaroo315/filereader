e45375ccbf4edca5db75998ae103dc606f2ed0fa
Jay <jay@Jays-MacBook-Air.local>
2019-06-08 23:16:15 -0500

    update

--- src/main/java/com/mac/main/Mac.java
+++ src/main/java/com/mac/main/Mac.java
@@ -3,7 +3,7 @@ package com.mac.main;
 
 public class Mac {
 
-    public static void main(String[]  args){
+    public static void main(String[] args) {
 
         // String , Integer , Boolean ,
 
@@ -12,13 +12,39 @@ public class Mac {
         Integer myInteger = 5;
 
         loopThroughString();
+        outputs("String args");
+
+        System.out.println("IS THIS A VALID AGE? "+isValidSmokingAge(17));
+    }
+
+    public static boolean isValidSmokingAge(int age){
+
+        for(int i = 0 ; i < age ; i++){
+            System.out.println(i);
+        }
+
+        if(age < 10){
+            return false;
+        }else if(age > 10 && age <17){
+            return false;
+        }else{
+            return true;
+        }
+
 
     }
 
     public static void loopThroughString() {
-//        for (){
-//
-//        }
-        System.out.println("this is working");
+
+        System.out.println("this is not working");
     }
+
+
+    public static void outputs(String args) {
+        int gil = 5;
+        int love = 7;
+        System.out.println(gil + love);
+    }
+
+
 }
